57c5e20a012dc27de0949f6519295808fa061eda
Gregory0511P <gregorypavlov0511@gmail.com>
2024-06-06 18:16:12 +0400

    change main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
+//import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import * as process from 'node:process';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.setGlobalPrefix('api');
 
-  const configService = app.get(ConfigService);
+  //const configService = app.get(ConfigService);
 
-  await app.listen(configService.get('PORT'), () =>
-    console.log(`BotServer запущен на порту ${configService.get('PORT')}`),
+  //await app.listen(configService.get('PORT'), () =>
+  //  console.log(`BotServer запущен на порту ${configService.get('PORT')}`),
+  //);
+  await app.listen(process.env.PORT, () =>
+    console.log(`BotServer запущен на порту ${process.env.PORT}`),
   );
 }
 bootstrap();
